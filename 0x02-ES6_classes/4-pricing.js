cd166b97c013c017402fc3e407bd03afe532405a
Adam Ibrahim <onelordad@gmail.com>
2022-12-22 23:35:40 +0100

    update

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+//  import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
